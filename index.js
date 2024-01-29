22a6786ee853b18e2bc7c591b374659631a9b1ca
Samuel Chu <samueljchu0126@gmail.com>
2024-01-29 15:13:59 -0500

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2; 
+let multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() *100) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = (num3 % num4);
+
+const max = Math.max(5, 10, 15, 20);
